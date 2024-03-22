14adf27e2edf3964e7c316748b13168005c9cad1
alexpresthus <alexander.presthus@gmail.com>
2024-03-22 11:39:12 +0100

    [frontend] Cleanup Nextjs files and add basic pages

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -1,10 +1,10 @@
-export default function Home() {
+export default function DashboardPage() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-8`}
     >
       <div className="max-w-5xl w-full items-center justify-center">
-        <h1 className="text-2xl font-bold text-center">Home</h1>
+        <h1 className="text-2xl font-bold text-center">Dashboard</h1>
       </div>
     </main>
   )
